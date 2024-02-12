104e3d059c748460926649c923089d9dc792e7ed
Hamidou Mamadou <midou88530@gmail.com>
2024-02-12 09:57:18 +0100

    Express 01.1

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-"// js"
+"// js";
 
-const express = require("express");
-
-const app = express();
+const app = require("./app");
 
 const port = 5000;
-
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
 
 app
   .listen(port, () => {
@@ -40,35 +11,3 @@ app
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
-
-const welcome = (req, res) => {
-    res.send("Welcome to my favourite movie list");
-  };
-  
-app.get("/", welcome);
-const getMovies = (req, res) => {
-    res.status(200).json(movies);
-  };
-  
-app.get("/api/movies", getMovies);
-
-
-const getMoviesById = (req, res) => {
-    const id = parseInt(req.params.id); 
-    let movieFound = null;
-
-    for(let i = 0; i < movies.length; i++) {
-        if(movies[i].id === id) {
-            movieFound = movies[i];
-            break;
-        }
-    }
-
-    if(movieFound) {
-        res.status(200).json(movieFound); 
-    } else {
-        res.status(404).send('Not Found');
-    }
-};
-
-app.get("/api/movies/:id", getMoviesById);
